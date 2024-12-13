b773e7253585dda01ba0fe5112308b1ba77363b2
buyath <buyaththayub@gmail.com>
2024-12-13 18:53:45 +0000

    array has been sorted

---
+++ sort_the_arr.java
@@ -0,0 +1,18 @@
+public class sort_the_arr{
+    public static void main(String[] args){
+        int[] arr = {5,3,4,1,2};
+        int temp;
+        for(int i = 0; i<arr.length; i++){
+            for(int j = 0; j<arr.length - 1; j++){
+                if(arr[j] > arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for(int num : arr){
+            System.out.println(num + " ");
+        }
+    }
+}
